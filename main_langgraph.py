ab7a3c3039cc656ac05febf8c4f5af262b2987a9
Diogo Coelho <diogooliveiracoelho@gmail.com>
2026-08-06 10:05:50 -0300

    Inclusão de langgraph'

---
+++ main_langgraph.py
@@ -0,0 +1,53 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import os
+from typing import Literal, TypedDict
+
+load_dotenv()
+api_key = os.getenv("API_OPEN_AI_TEST_KEY")
+
+modelo = ChatOpenAI(
+  model="gpt-4o-mini",  
+  temperature=0.5,
+  api_key=api_key
+)
+
+prompt_consultor_praia = ChatPromptTemplate.from_messages(
+  [
+    ("system", "Apresentes-se como Sra. Praia. Você é uma especialista em viagens com destinos para praias."),
+    ("human", "{query}")
+  ]
+)
+
+prompt_consultor_montanha = ChatPromptTemplate.from_messages(
+  [
+    ("system", "Apresentes-se como Sra. Montanha. Você é uma especialista em viagens com destinos para montanhas e atividades radicais."),
+    ("human", "{query}")
+  ]
+)
+
+cadeia_praia = prompt_consultor_praia | modelo | StrOutputParser()
+
+cadeia_montanha = prompt_consultor_montanha | modelo | StrOutputParser()
+
+class Rota(TypedDict):
+  destino: Literal["praia", "montanha"]
+
+prompt_roteador = ChatPromptTemplate.from_messages(
+  [
+    ("system", "Responda apenas com 'praia' ou 'montanha'"),
+    ("human", "{query}")
+  ]
+)
+
+roteador = prompt_roteador | modelo.with_structured_output(Rota)
+
+def responda(pergunta: str):
+  rota = roteador.invoke({"query": pergunta})["destino"]
+  if rota == "praia":
+    return cadeia_praia.invoke({"query": pergunta})
+  return cadeia_montanha.invoke({"query": pergunta})
+
+print(responda("Quero passear por praias belas no Brasil."))
